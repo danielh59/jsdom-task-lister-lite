f76190dc40d101109f3a3bdfd2439d2fbd28fa5f
tak59 <tak59@live.com>
2021-04-07 18:58:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    document.getElementById("output-box").innerHTML += "Will Not Submit";
+    event.preventDefault();
+}, false);
 });
